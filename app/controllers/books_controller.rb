0cd70a2e6caa4d77ebffca3f14b51cafcb8d55a7
root <root@localhost>
2022-09-16 03:04:00 +0000

    Add unit and integration tests for Author, Price, and Date

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -57,6 +57,9 @@ class BooksController < ApplicationController
     end
   end
 
+  def details
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_book
--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -1,5 +1,6 @@
 # location: spec/unit/unit_spec.rb
 # require 'rails_helper'
+require 'Date'
 
 RSpec.describe Book, type: :model do
   subject do
@@ -12,6 +13,51 @@ RSpec.describe Book, type: :model do
 
   it 'is not valid without a name' do
     subject.title = nil
+    expect(subject).not_to be_valid
+  end
+end
+
+RSpec.describe Book, type: :model do
+  subject do
+    described_class.new(author: "JK Rowling")
+  end
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without an author' do
+    subject.author = nil
+    expect(subject).not_to be_valid
+  end
+end
+
+RSpec.describe Book, type: :model do
+  subject do
+    described_class.new(price: 28.99)
+  end
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without a price' do
+    subject.price = nil
+    expect(subject).not_to be_valid
+  end
+end
+
+RSpec.describe Book, type: :model do
+  subject do
+    described_class.new(publish_date: Date.new(2001,2,25))
+  end
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without a published date' do
+    subject.publish_date = nil
     expect(subject).not_to be_valid
   end
 end
